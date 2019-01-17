ca24b27946a820caaf5779d01bff6bbe2e31850d
Wenqi He <hewenqi96@gmail.com>
2019-01-17 02:24:53 -0500

    Read id from query string

--- server.go
+++ server.go
@@ -1,54 +1,49 @@
 package main
 
 import (
-	"net/http"
-  "strings"
+  "net/http"
   "fmt"
+  "strings"
   "database/sql"
-	_ "github.com/go-sql-driver/mysql"
-
+  _ "github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
 
 func test(w http.ResponseWriter, r *http.Request) {
+  query := r.URL.Query()
+  id := strings.Join(query["id"], "")
+
   var (
-    id int
     start string
     end string
   )
-  rows, err := db.Query("select * from records")
-  if err != nil {
-  	panic(err)
-  }
+  rows, err := db.Query("select start_time, end_time from records where user_id = ?", id)
+  if err != nil { panic(err) }
   defer rows.Close()
   for rows.Next() {
-  	err := rows.Scan(&id, &start, &end)
-  	if err != nil {
-  		panic(err)
-    }
-    fmt.Printf("%d, %s, %s", id, start, end);
+  	err := rows.Scan(&start, &end)
+  	if err != nil { panic(err) }
+    fmt.Printf("%s, %s\n", start, end);
   }
-  err = rows.Err()
-  if err != nil {
-  	panic(err)
-  }
-  message := r.URL.Path
-  message = strings.TrimPrefix(message, "/")
-  message = "Hello " + message
+  if rows.Err() != nil { panic(err) }
+  
+  message := start + " => " + end + "\n"
   w.Write([]byte(message))
-  fmt.Printf("yo\n")
 }
 
 func main() {
   var err error
   db, err = sql.Open("mysql", "root@tcp(127.0.0.1:3306)/sitting_time_tracker")
   if err != nil { panic(err) }
-  if err = db.Ping(); err != nil { fmt.Printf("YOYO") }
+  if err = db.Ping(); err == nil {
+    fmt.Printf("Database Connected!\n")
+  }
   defer db.Close()
 
   http.HandleFunc("/test", test)
-	if err = http.ListenAndServe(":8080", nil); err != nil {
+
+  if err = http.ListenAndServe(":8080", nil); err != nil {
     panic(err)
   }
 }
